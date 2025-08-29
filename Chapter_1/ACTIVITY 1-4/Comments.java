b0b246ea0aa6d65551b1916cfa85cdfb1d58ffb9
EarlSiegfredCarmelotes-UCM <EarlSiegfred15@gmail.com>
2025-08-29 16:36:16 +0800

    Add files via upload

---
+++ Chapter_1/ACTIVITY 1-4/Comments.java
@@ -0,0 +1,15 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // Single-line comment
+        System.out.println("This is a single-line comment");
+
+        /* Multi-line comment */
+        System.out.println("This is a Multi-line comment");
+
+        /** Documentation comment  */
+        System.out.println("This is a Documatation comment");
+
+    }
+    
+}
---
+++ Chapter_1/ACTIVITY 1-4/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+
+         System.out.println("If I could, then I would");
+         System.out.println("I'll go wherever you will go");
+         System.out.println("Way up high or down low");
+         System.out.println("I'll go wherever you will go");
+
+    }
+    
+}
