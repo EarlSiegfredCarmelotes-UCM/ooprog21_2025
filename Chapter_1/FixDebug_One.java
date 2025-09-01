070375c2b2f64bf841c3de75eabc415fade54fb3
EarlSiegfredCarmelotes-UCM <EarlSiegfred15@gmail.com>
2025-09-01 13:29:40 +0800

    Add files via upload

---
+++ Chapter_1/FixDebug_One.java
@@ -0,0 +1,16 @@
+public class FixDebug_One {
+   public static void main(String[] args){
+
+      System.out.println("Hello World!");    
+
+   }
+}
+
+/* Bugs:
+ * 1. In java, you can't put space in a class name. once your class name is separated in two words, it will cause error.
+ * 2. Input "main" (lowercase) instead of "Main".
+ * 3. String needs to have both an open and close square parenthesis ( [] ).
+ * 4. It has missing "e" in ( Systm.out.println ).
+ * 5. No semi colon in the end of ("Hello World!").
+ * 6. No closing bracket in the end of the code ( } ).
+ */
