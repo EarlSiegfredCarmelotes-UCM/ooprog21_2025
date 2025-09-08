b6274a341617d4c0a8f8a6ec534f6baecc4e9770
EarlSiegfredCarmelotes-UCM <EarlSiegfred15@gmail.com>
2025-09-08 16:31:30 +0800

    Add files via upload

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class UsingJOptionPaneDialogs {
+
+     public static void main(String[] args) {
+
+            while(true) {
+
+            String name = JOptionPane.showInputDialog(null, "What is your name?");
+
+            int choice = JOptionPane.showConfirmDialog(null, "Do you want to display your name?");
+
+            if (choice == JOptionPane.YES_NO_OPTION) {
+
+                JOptionPane.showMessageDialog(null, "Hello, " + name);
+                break;
+            }
+         }
+
+     }
+    
+}
