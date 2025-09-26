866bec68a2c3f0ecf0b6643b769bcfc852478ddb
EarlSiegfredCarmelotes-UCM <EarlSiegfred15@gmail.com>
2025-09-26 10:02:16 +0800

    Add files via upload

---
+++ Chapter_4/DebugFour2.java
@@ -0,0 +1,39 @@
+// This class uses a DebugPen class
+// to instantiate two Pen objects
+// One with default values
+// and the other with user-supplied values
+
+import java.util.Scanner;
+public class DebugFour2
+{
+       public static void main(String args[])
+{
+    Scanner input = new Scanner(System.in);
+       String color;
+       String point;
+       DebugPen pen1;
+       DebugPen pen2;
+
+    System.out.print("Enter a color ink for the pen >> ");
+       color = input.nextLine();
+
+    System.out.print("Enter a point size - fine, medium, or thick >> ");
+       point = input.nextLine();
+       pen1 = new DebugPen();
+       pen2 = new DebugPen(point, color);
+
+    System.out.println("Default value pen:");
+       display(pen1);
+
+    System.out.println("User value pen:");
+       display(pen2);
+}
+    public static void display(DebugPen pen)
+{
+
+    System.out.println(" The pen has ink color " + pen.getColor());
+    System.out.println(" and a " + pen.getPoint());
+}
+
+}
+
